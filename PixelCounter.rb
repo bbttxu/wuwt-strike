dd96a27612664c9b05c186f8653d3698d1e09cf6
bbttxu <bbttxu@gmail.com>
2010-09-19 16:32:34 -0500

    more tests

--- PixelCounter.rb
+++ PixelCounter.rb
@@ -5,16 +5,64 @@ include Magick
 
 class PixelCounter
   
-  def initialize( image, values )
-    process( image, values )
+  def initialize
+
+  end
+
+
+  def cumulative_weight_freq( hash )
+    # weight_frequency_sorted = histogram.sort{ |x,y| y <=> x }
+    weighted = 0
+    sum = 0
+    hash.each do |weight,freq|
+      sum += freq
+      weighted += weight.to_f * freq
+    end
+    return weighted / sum
+  end
+
+  def cumulative_pixels( input )
+    sum = 0
+    input.each do |weight,freq|
+      sum += freq.abs
+    end
+    return sum
   end
   
-  def process( image, values )
-    puts values
-    # image = Magick::ImageList.new( image )
+
+  def process( path, values )
+    image = Magick::ImageList.new( path )    
+    histogram = image.color_histogram
     
-    # histogram = image.color_histogram
+    output = {}
+    
+    frequencies = {}
+    histogram.each do | rgba, freq |
+      if rgba.opacity == 0
+        color = rgba.to_color
+        if values[color]
+          # puts color
+          # output[color] += freq.to_f
+          # puts values[color]
+          # puts freq
+          # puts ( values[color] * freq )
+          
+          # counted_pixels += freq
+          # weighted_average += ( freq * values[color] )
+          
+          current_value = output[values[color]] || 0
+          output[values[color]] = current_value + freq
 
-
+          current_freq = frequencies[freq] || 0
+          frequencies[values[color]] = current_freq + freq
+          
+        else
+          # puts color
+        end
+      end      
+    end
+    output['pixels'] = cumulative_pixels frequencies
+    output['average'] = cumulative_weight_freq frequencies
+    return output
   end
 end
